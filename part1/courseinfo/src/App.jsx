40764303dd9f9f3bd770bb9eda6bb800d069682d
ZebraSistemas <110634700+migueldr12@users.noreply.github.com>
2025-11-05 17:42:35 -0600

    Excercise 1.2

--- part1/courseinfo/src/App.jsx
+++ part1/courseinfo/src/App.jsx
@@ -1,3 +1,11 @@
+const Part = (props) => {
+  return (
+    <p>
+      {props.part} {props.exercises}
+    </p>
+  );
+};
+
 const Header = (props) => {
   return <h1>{props.course}</h1>;
 };
@@ -5,25 +13,15 @@ const Header = (props) => {
 const Content = (props) => {
   return (
     <>
-      <p>
-        {props.part1} {props.exercises1}
-      </p>
-      <p>
-        {props.part2} {props.exercises2}
-      </p>
-      <p>
-        {props.part3} {props.exercises3}
-      </p>
+      <Part part={props.part1} exercises={props.exercises1} />
+      <Part part={props.part2} exercises={props.exercises2} />
+      <Part part={props.part3} exercises={props.exercises3} />
     </>
   );
 };
 
 const Total = (props) => {
-  return (
-    <>
-      <p>Number of exercises {props.total}</p>
-    </>
-  );
+  return <p>Number of exercises {props.total}</p>;
 };
 const App = () => {
   const course = "Half Stack application development";
